fd106515af374545b6904cf35a371fdf3e98c90a
fedefrusc <fedefrusc95@gmail.com>
2017-12-20 15:22:05 +0100

    Update config.js

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,11 +1,11 @@
 
 
-/* // Dev
+ // Dev da decommentare in ambiente di sviluppo
 module.exports = {
     "serverUrl": "http://localhost:4000"
-} */
+} 
 
-//Prod
-module.exports = {
-    "serverUrl": "https://mpr75aa5n1.execute-api.eu-west-1.amazonaws.com/dev"
-}
+//Prod, da decommentare in ambiente si produzione
+//module.exports = {
+//    "serverUrl": "https://mpr75aa5n1.execute-api.eu-west-1.amazonaws.com/dev"
+//}
